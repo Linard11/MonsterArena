8b518bb942dd5685febeced125a8cc7b1b4cd5f0
Linard Hug <linard.hug@googlemail.com>
2022-09-11 18:49:51 +0200

    Add monster title and health fields. And give it maxHealth on game start

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    [SerializeField] private string title = "Monster";
+    [SerializeField] private float maxHealth = 10f;
     
+    private float currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
 }
