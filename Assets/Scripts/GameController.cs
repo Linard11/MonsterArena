93d3e9f4de9771ae1b30a6cd4892dc266a09797b
Linard Hug <linard.hug@googlemail.com>
2022-09-11 19:30:36 +0200

    Implement attack and health change logic

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -65,6 +65,8 @@ public class GameController : MonoBehaviour
             defender = monsterA;
             defenderUI = monsterAUI;
         }
+
+        attacker.Attack(defender);
     }
 
 
--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -4,7 +4,12 @@ public class Monster : MonoBehaviour
 {
     [SerializeField] private string title = "Monster";
     [SerializeField] private float maxHealth = 10f;
+
+    [Space]
     
+    [SerializeField] private string attackName = "ATTACKENNAME";
+    [SerializeField] private float attackStrength = 2f;
+
     private float currentHealth;
 
     private void Awake()
@@ -12,6 +17,23 @@ public class Monster : MonoBehaviour
         currentHealth = maxHealth;
     }
     
+    public void Attack(Monster target)
+    {
+        float damage = attackStrength;
+        target.UpdateHealth(-damage);
+    }
+    
+    public bool HasFainted()
+    {
+        return currentHealth == 0;
+    }
+
+    private void UpdateHealth(float change)
+    {
+        currentHealth += change;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+
     public string GetTitle()
     {
         return title;
