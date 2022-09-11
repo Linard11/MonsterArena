08090da43f85e0bc46418b86632988b1f3a6b0ca
Linard Hug <linard.hug@googlemail.com>
2022-09-11 18:57:07 +0200

    Create reference fields for the monster and their uis in the gamecontroller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,14 @@
+using TMPro;
+
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    
+    [SerializeField] private Monster monsterA;
+    [SerializeField] private Monster monsterB;
+
+    [SerializeField] private MonsterUI monsterAUI;
+    [SerializeField] private MonsterUI monsterBUI;
+
+    [SerializeField] private TextMeshProUGUI commentaryText;
 }
