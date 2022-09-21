d174c46cee2c82f4be57ebc7ebf7c01f83956b77
Linard Hug <linard.hug@googlemail.com>
2022-09-21 20:56:48 +0200

    Progress from lecture

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 using TMPro;
 
 using UnityEngine;
@@ -5,15 +7,22 @@ using UnityEngine.InputSystem;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private Monster monsterA;
-    [SerializeField] private Monster monsterB;
+    [SerializeField] private List<Monster> monsterPrefabs;
 
+    [SerializeField] private Transform monsterSlotA;
+    [SerializeField] private Transform monsterSlotB;
+    
     [SerializeField] private MonsterUI monsterAUI;
     [SerializeField] private MonsterUI monsterBUI;
 
     [SerializeField] private TextMeshProUGUI commentaryText;
 
+    private Monster monsterA;
+    private Monster monsterB;
+    
     private GameInput input;
+
+    private bool isMonsterATurn = true;
     
     private void Awake()
     {
@@ -28,10 +37,7 @@ public class GameController : MonoBehaviour
 
     private void Start()
     {
-        RegisterNewMonster(monsterA, monsterAUI);
-        RegisterNewMonster(monsterB, monsterBUI);
-        
-        commentaryText.SetText($"{monsterA.GetTitle()} trifft auf {monsterB.GetTitle()}!");
+        StartNewBattle();
     }
 
     private void OnDisable()
@@ -44,17 +50,86 @@ public class GameController : MonoBehaviour
         input.Player.Next.performed -= PerformNextAction;
     }
 
-    private void PerformNextAction(InputAction.CallbackContext context)
+    private void StartNewBattle()
     {
-        Debug.Log("Enter Taste wurde gedrückt.");
-        commentaryText.SetText("Nächste Aktion.");
-        // TODO Implement attacking
+        // Select a random number between 0 and the element count of the monster prefab list (exclusive).
+        int challengerAIndex = Random.Range(0, monsterPrefabs.Count);
+        int challengerBIndex = Random.Range(0, monsterPrefabs.Count);
+        
+        // Try again if picking the same two monsters.
+        if (monsterPrefabs.Count >= 2)
+        {
+            while (challengerAIndex == challengerBIndex)
+            {
+                challengerBIndex = Random.Range(0, monsterPrefabs.Count);
+            }
+        }
+
+        // Use the selected number to pick the corresponding monster.
+        Monster challengerA = monsterPrefabs[challengerAIndex];
+        Monster challengerB = monsterPrefabs[challengerBIndex];
+
+        monsterA = RegisterNewMonster(challengerA, monsterSlotA, monsterAUI);
+        monsterB = RegisterNewMonster(challengerB, monsterSlotB, monsterBUI);
+
+        commentaryText.SetText($"{monsterA.GetTitle()} trifft auf {monsterB.GetTitle()}!");
     }
 
-    private void RegisterNewMonster(Monster monster, MonsterUI monsterUI)
+    private void PerformNextAction(InputAction.CallbackContext context)
     {
-        UpdateTitle(monster, monsterUI);
-        UpdateHealth(monster, monsterUI);
+        if (monsterA.HasFainted() || monsterB.HasFainted())
+        {
+            StartNewBattle();
+            return;
+        }
+
+        Monster attacker;
+        Monster defender;
+        MonsterUI defenderUI;
+        
+        if (isMonsterATurn) // Monster A greift an
+        {
+            attacker = monsterA;
+            defender = monsterB;
+            defenderUI = monsterBUI;
+        }
+        else // Monster B greift an
+        {
+            attacker = monsterB;
+            defender = monsterA;
+            defenderUI = monsterAUI;
+        }
+
+        string attackDescription = attacker.Attack(defender);
+        UpdateHealth(defender, defenderUI);
+        
+        commentaryText.SetText(attackDescription);
+
+        isMonsterATurn = !isMonsterATurn;
+    }
+
+    private Monster RegisterNewMonster(Monster monsterPrefab, Transform monsterSlot, MonsterUI monsterUI)
+    {
+        // Remove old monster in slots.
+        ClearSlot(monsterSlot);
+        
+        // Spawn monster prefab.
+        Monster newSpawned = Instantiate(monsterPrefab, monsterSlot);
+
+        // Link the UI to the monster.
+        UpdateTitle(newSpawned, monsterUI);
+        UpdateHealth(newSpawned, monsterUI);
+
+        return newSpawned;
+    }
+
+    private void ClearSlot(Transform slot)
+    {
+        for (int i = slot.childCount - 1; i >= 0; i--)
+        {
+            Transform child = slot.GetChild(i);
+            Destroy(child.gameObject);
+        }
     }
 
     private void UpdateTitle(Monster monster, MonsterUI monsterUI)
--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,17 +1,72 @@
-using System;
-
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Monster : MonoBehaviour
 {
     [SerializeField] private string title = "Monster";
-    [SerializeField] private float maxHealth = 10f;
 
+    [Header("Attack")]
+    [SerializeField] private string attackName = "ATTACKENNAME";
+    [SerializeField] private float attackStrength = 2f;
+    [SerializeField] private GameObject attackEffect;
+
+    [Header("Health")]
+    [SerializeField] private float maxHealth = 10f;
+    [SerializeField] private UnityEvent onDamaged;
+    [SerializeField] private UnityEvent onFainted;
+    
     private float currentHealth;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+    }
+
+    public string Attack(Monster target)
+    {
+        float damage = attackStrength;
+        target.UpdateHealth(-damage);
+
+        Instantiate(attackEffect, target.transform.position, Quaternion.identity);
+
+        return GetAttackString(target, damage);
+    }
+
+    public bool HasFainted()
+    {
+        return currentHealth == 0;
+    }
+
+    private void UpdateHealth(float change)
+    {
+        float newHealth = currentHealth + change;
+        currentHealth = Mathf.Clamp(newHealth, 0, maxHealth);
+
+        if (change < 0)
+        {
+            if (HasFainted())
+            {
+                onFainted.Invoke();
+            }
+            else
+            {
+                onDamaged.Invoke();
+            }
+        }
+    }
+
+    private string GetAttackString(Monster target, float damage)
+    {
+        string text = $"{this.title} wendet {this.attackName} an.";
+
+        text += $" {target.title} erleidet {damage} Schaden.";
+
+        if (target.HasFainted())
+        {
+            text += $" {target.title} ist ohnmächtig geworden.";
+        }
+
+        return text;
     }
 
     public string GetTitle()
