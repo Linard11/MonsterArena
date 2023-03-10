34f1c1d41f123f2109951a8391f3a0bfcaf7f734
Linard Hug <linard.hug@googlemail.com>
2023-03-10 13:05:52 +0100

    Give Monster health and update UI

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+using TMPro;
+
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Monster monsterA;
+    [SerializeField] private Monster monsterB;
+
+    [SerializeField] private MonsterUI monsterAUI;
+    [SerializeField] private MonsterUI monsterBUI;
+
+    [SerializeField] private TextMeshProUGUI commentaryText;
+
+    private void Start()
+    {
+        RegisterNewMonster(monsterA, monsterAUI);
+        RegisterNewMonster(monsterB, monsterBUI);
+        
+        commentaryText.SetText($"{monsterA.GetTitle()} trifft auf {monsterB.GetTitle()}!");
+    }
+
+    private void RegisterNewMonster(Monster newMonster, MonsterUI newMonsterUI)
+    {
+        UpdateTitle(newMonster, newMonsterUI);
+        UpdateHealth(newMonster, newMonsterUI);
+    }
+    
+    private void UpdateTitle(Monster newMonster, MonsterUI newMonsterUI)
+    {
+        newMonsterUI.UpdateTitle(newMonster.GetTitle());
+    }
+    
+    private void UpdateHealth(Monster newMonster, MonsterUI newMonsterUI)
+    {
+        newMonsterUI.UpdateHealth(newMonster.GetCurrentHealth(), newMonster.GetMaxHealth());
+    }
+}
