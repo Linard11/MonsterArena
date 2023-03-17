82fe320216f6423c8fe868e3948a64b4ad899b99
Linard Hug <linard.hug@googlemail.com>
2023-03-17 13:12:19 +0100

    Finished Monsterarena

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,29 +1,122 @@
+using System.Collections.Generic;
+
 using TMPro;
 
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private List<Monster> monsterPrefabs;
+
+    [SerializeField] private Transform monsterSlotA;
+    [SerializeField] private Transform monsterSlotB;
+
     [SerializeField] private MonsterUI monsterAUI;
     [SerializeField] private MonsterUI monsterBUI;
 
-    [SerializeField] private Monster monsterA;
-    [SerializeField] private Monster monsterB;
-
     [SerializeField] private TextMeshProUGUI commentaryText;
+    
+    private Monster monsterA;
+    private Monster monsterB;
+
+    private GameInput input;
+
+    private bool isMonsterATurn = true;
+    
+    private void Awake()
+    {
+        input = new GameInput();
+        input.Player.Next.performed += PerformNextAction;
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
 
     private void Start()
     {
-        RegisterNewMonster(monsterA, monsterAUI);
-        RegisterNewMonster(monsterB, monsterBUI);
+        StartNewBattle();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        input.Player.Next.performed -= PerformNextAction;
+    }
+
+    private void PerformNextAction(InputAction.CallbackContext context)
+    {
+        if (monsterA.HasFainted() || monsterB.HasFainted())
+        {
+            StartNewBattle();
+            return;
+        }
+
+        Monster attacker;
+        Monster defender;
+        MonsterUI defenderUI;
+
+        if (isMonsterATurn) // Monster A greift an
+        {
+            attacker = monsterA;
+            defender = monsterB;
+            defenderUI = monsterBUI;
+        }
+        else // Monster B greift an
+        {
+            attacker = monsterB;
+            defender = monsterA;
+            defenderUI = monsterAUI;
+        }
+        
+        attacker.Attack(defender);
+        UpdateHealth(defender, defenderUI);
+
+        isMonsterATurn = !isMonsterATurn;
+    }
+
+    private void StartNewBattle()
+    {
+        int challengerAIndex = Random.Range(0, monsterPrefabs.Count);
+        int challengerBIndex = Random.Range(0, monsterPrefabs.Count);
+        
+        // TODO Generate new second random number for B if the same as A
+        // Tip: Try a while loop
+
+        Monster challengerA = monsterPrefabs[challengerAIndex];
+        Monster challengerB = monsterPrefabs[challengerBIndex];
+        
+        monsterA = RegisterNewMonster(challengerA, monsterSlotA, monsterAUI);
+        monsterB = RegisterNewMonster(challengerB, monsterSlotB, monsterBUI);
         
         commentaryText.SetText($"{monsterA.GetTitle()} trifft auf {monsterB.GetTitle()}!");
     }
 
-    private void RegisterNewMonster(Monster newMonster, MonsterUI newMonsterUI)
+    private Monster RegisterNewMonster(Monster monsterPrefab, Transform monsterSlot, MonsterUI newMonsterUI)
     {
-        UpdateTitle(newMonster, newMonsterUI);
-        UpdateHealth(newMonster, newMonsterUI);
+        ClearSlot(monsterSlot);
+
+        Monster newSpawned = Instantiate(monsterPrefab, monsterSlot);
+        
+        UpdateTitle(newSpawned, newMonsterUI);
+        UpdateHealth(newSpawned, newMonsterUI);
+
+        return newSpawned;
+    }
+
+    private void ClearSlot(Transform slot)
+    {
+        for (int i = slot.childCount - 1; i >= 0; i--)
+        {
+            Transform child = slot.GetChild(i);
+            Destroy(child.gameObject);
+        }
     }
     
     private void UpdateTitle(Monster newMonster, MonsterUI newMonsterUI)
--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -5,11 +5,43 @@ public class Monster : MonoBehaviour
     [SerializeField] private string title = "Monster";
     [SerializeField] private float maxHealth = 10f;
 
+    [SerializeField] private string attackName = "ATTACKENNAME";
+    [SerializeField] private float attackStrength = 2f;
+    
+    // TODO Use Unity Events for Animations "TakeDamage" und "Fainting" im Animator Controller
+    // onDamaged
+    // onFainted
+    // Invoke in UpdateHealth()
+
     private float currentHealth;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+    }
+
+    public void Attack(Monster target)
+    {
+        float damage = attackStrength;
+        // TODO use multiplier 0.5x and 2.0x for damage depending how which element attacks which element
+        target.UpdateHealth(-damage);
+        
+        // TODO Spawn particle effect Prefab over target
+        // Tip: Make a inspector field for the prefab
+        
+        // TODO Create string text to show who attacked who with how much damage
+        // and display on commentaryText
+    }
+
+    public bool HasFainted()
+    {
+        return currentHealth == 0;
+    }
+
+    private void UpdateHealth(float change)
+    {
+        currentHealth += change;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
     }
 
     public string GetTitle()
