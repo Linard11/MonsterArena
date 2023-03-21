1acd71744d42d8e6e653ea9bb69a8b81bdda4203
Linard Hug <linard.hug@googlemail.com>
2023-03-21 15:51:31 +0100

    Finish MonsterArena

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,5 +1,3 @@
-using System;
-
 using UnityEngine;
 
 public class Monster : MonoBehaviour
@@ -7,11 +5,44 @@ public class Monster : MonoBehaviour
     [SerializeField] private string title = "Monster";
     [SerializeField] private float maxHealth = 10f;
 
+    [SerializeField] private string attackName = "ATTACKENNAME";
+    [SerializeField] private float attackStrength = 2f;
+    
     private float currentHealth;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+    }
+
+    public void Attack(Monster target)
+    {
+        float damage = attackStrength;
+        // TODO Add multiplier 0.5x or 2.0x depending on elements of attacker and defender.
+        // Tip: Use a enum
+        
+        // TODO Spawn attack particle system above target
+        
+        // TODO Create a attack description and set it in the commentaryText
+
+        target.UpdateHealth(-damage);
+    }
+
+    public bool HasFainted()
+    {
+        return currentHealth == 0;
+    }
+
+    private void UpdateHealth(float change)
+    {
+        currentHealth += change;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        
+        // TODO Play TakeDamage or Fainting animation
+        // Tip: Use UnityEvents
+        // onDamaged
+        // onFainting
+        
     }
 
     public string GetTitle()
